081c56207b709b48b3072ee73f929a6cdfcbe513
J-Obog <jobogbaimhe@gmail.com>
2025-11-22 22:45:43 -0500

    some enhancements

--- compute.js
+++ compute.js
@@ -4,6 +4,17 @@ const GUESSES = ["brass", "barns", "brain"];
 
 const gameAppRoot = window.document.getElementsByTagName("game-app")[0]; 
 const gameKeyboardRoot = gameAppRoot.shadowRoot.querySelector("game-keyboard"); 
+const gameThemeManagerRoot = gameAppRoot.shadowRoot.querySelector("game-theme-manager");
+
+const board = gameThemeManagerRoot.querySelector("#board-container");
+
+
+// Create the new element you want to add
+const newChild = document.createElement('div');
+newChild.innerHTML = 'The word to guess is <b>BRAIN</b>';
+
+// Append the new child as the first child of the parent
+board.prepend(newChild);
 
 gameKeyboardRoot.style.display = 'none'; 
 
